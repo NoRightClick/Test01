37f1a8b79ae25d18066c8d52eaa1719758566873
Martin Wilson <wilson91.mc@gmail.com>
2015-08-27 21:03:59 -0600

    Created the basic code

---
+++ Testing/Testing/MW_Test_01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string ok = "Everything is running fine";
+	string done = "This is just a test, don't worry, things will get more complex";
+
+	cout << ok << endl;
+	cout << done << endl;
+	
+	// if you can change this, please write something below this comment.
+
+
+
+	return 0;
+}
